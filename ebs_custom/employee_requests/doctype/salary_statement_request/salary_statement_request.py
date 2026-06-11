3212de0da532a614787690d1344f3498880cb610
SyedKashif2646 <skashifshah129@gmail.com>
2026-06-11 15:51:01 +0500

    create doctype for salary statement request

---
+++ ebs_custom/employee_requests/doctype/salary_statement_request/salary_statement_request.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2026, Arslan and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class SalaryStatementRequest(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		amended_from: DF.Link | None
+		company: DF.Link | None
+		date_of_joining: DF.Date | None
+		designation: DF.Link | None
+		employee: DF.Link
+		employee_name: DF.Data | None
+		purpose: DF.SmallText
+		remarks: DF.SmallText | None
+		request_date: DF.Date
+		salary_statement: DF.Attach | None
+		status: DF.Literal["Pending", "Processed"]
+	# end: auto-generated types
+
+	pass
