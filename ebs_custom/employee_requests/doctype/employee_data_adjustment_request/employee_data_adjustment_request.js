a79d63d68e8643fc88239bbddd667def9c9a72eb
SyedKashif2646 <skashifshah129@gmail.com>
2026-06-17 17:31:54 +0500

    fix pdf print and doctype

--- ebs_custom/employee_requests/doctype/employee_data_adjustment_request/employee_data_adjustment_request.js
+++ ebs_custom/employee_requests/doctype/employee_data_adjustment_request/employee_data_adjustment_request.js
@@ -2,56 +2,127 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Employee Data Adjustment Request", {
+
     onload: function(frm) {
-        // Dynamic Employee fields fetch karne ka method
         frm.trigger("set_employee_fields");
     },
-    
-    set_employee_fields: function(frm) {
-        // Ye method dynamic fields ko fetch karega
-        frappe.model.with_doctype("Employee", function() {
-            var fields = frappe.meta.get_docfields("Employee");
-            var options = [""];
-            
-            $.each(fields, function(i, field) {
-                // Dynamic fields filter (jo bhi Employee doctype mein add honge woh auto include honge)
-                if (field.fieldtype && field.label && 
-                    field.fieldtype != "Section Break" && 
-                    field.fieldtype != "Column Break" && 
-                    field.fieldtype != "Tab Break" &&
-                    field.fieldtype != "Button" &&
-                    field.fieldtype != "HTML" &&
-                    field.fieldtype != "Attach" &&
-                    field.fieldtype != "Image") {
-                    
-                    options.push(field.label);
-                }
-            });
-            
-            console.log("Dynamic Fields Loaded:", options.length);
-            
-            // Child table mein options set karein
-            if (frm.fields_dict["items"]) {
-                frm.fields_dict["items"].grid.update_docfield_property(
-                    "field_to_be_updated", 
-                    "options", 
-                    options
-                );
-            }
-        });
-    },
-    
+
     refresh: function(frm) {
         if (frm.doc.__islocal) {
             frm.trigger("set_employee_fields");
         }
+    },
+
+    set_employee_fields: function(frm) {
+
+        frappe.model.with_doctype("Employee", function() {
+
+            let fields = frappe.meta.get_docfields("Employee");
+            let options = [""];
+            let field_map = {};
+
+            $.each(fields, function(i, field) {
+
+                if (
+                    field.fieldtype &&
+                    field.label &&
+                    !field.hidden &&
+                    !field.read_only &&
+                    field.fieldtype !== "Check" &&
+                    ![
+                        "Section Break",
+                        "Column Break",
+                        "Tab Break",
+                        "Button",
+                        "HTML",
+                        "Image",
+                        "Attach Image",
+                        "Series",
+                        "Table",
+                        "Table MultiSelect",
+                        "Fold",
+                        "Heading"
+                    ].includes(field.fieldtype) &&
+                    ![
+                        "name",
+                        "naming_series",
+                        "owner",
+                        "creation",
+                        "modified",
+                        "modified_by",
+                        "docstatus",
+                        "idx",
+                        "lft",
+                        "rgt",
+                        "old_parent"
+                    ].includes(field.fieldname)
+                ) {
+
+                    // Show only label in dropdown
+                    options.push(field.label);
+
+                    // Store label -> fieldname mapping
+                    field_map[field.label] = field.fieldname;
+                }
+            });
+
+            // Save mapping on form
+            frm.employee_field_map = field_map;
+
+            console.log("Field Map:", field_map);
+
+            if (frm.fields_dict["items"]) {
+                frm.fields_dict["items"].grid.update_docfield_property(
+                    "field_to_be_updated",
+                    "options",
+                    options
+                );
+            }
+        });
     }
 });
 
-// Child table mein dynamic options set karne ke liye
 frappe.ui.form.on("Employee Adjustment Items", {
-    before_items_add: function(frm, cdt, cdn) {
-        // Naya row add karne se pehle options set karein
+
+    before_items_add: function(frm) {
         frm.trigger("set_employee_fields");
+    },
+
+    field_to_be_updated: function(frm, cdt, cdn) {
+
+        let row = locals[cdt][cdn];
+
+        if (!frm.doc.employee) {
+            frappe.msgprint("Please select Employee first");
+            return;
+        }
+
+        if (row.field_to_be_updated) {
+
+            let fieldname =
+                frm.employee_field_map &&
+                frm.employee_field_map[row.field_to_be_updated];
+
+            if (!fieldname) {
+                return;
+            }
+
+            console.log("Selected Label:", row.field_to_be_updated);
+            console.log("Actual Fieldname:", fieldname);
+
+            frappe.db.get_value(
+                "Employee",
+                frm.doc.employee,
+                fieldname
+            ).then(r => {
+
+                console.log("DB Response:", r);
+
+                if (r.message) {
+                    row.current_value = r.message[fieldname] || "";
+                    frm.refresh_field("items");
+                }
+            });
+        }
     }
 });
